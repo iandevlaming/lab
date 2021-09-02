9dd25d1030f4f01f5027553a6908b100e2e7e744
ian <iandevlaming@gmail.com>
2021-09-02 16:33:00 -0600

    old stuff

--- search/include/search/dijkstra.hpp
+++ search/include/search/dijkstra.hpp
@@ -8,6 +8,68 @@
 
 namespace search
 {
+template <typename X,
+          typename IsGoalF,
+          typename NeighborsF,
+          typename Q = std::unordered_set<X>,
+          // typename PushF = decltype(bfs_impl::push<X>),
+          // typename PopF = decltype(bfs_impl::pop<X>),
+          // typename IsEmptyF = decltype(bfs_impl::isEmpty<X>),
+          // typename P = std::unordered_map<X, std::pair<X, int>>,
+          // typename VisitF = decltype(search_impl::visit<X>),
+          // typename IsVisitedF = decltype(search_impl::isVisited<X>),
+          typename = has_signature<bool, IsGoalF, X>,
+          typename = has_signature<std::vector<X>, NeighborsF, X>,
+          // typename = has_signature<void, PushF, Q &, X>,
+          // typename = has_signature<X, PopF, Q &>,
+          // typename = has_signature<bool, IsEmptyF, Q const &>,
+          // typename = has_signature<void, VisitF, P &, X, X>,
+          // typename = has_signature<bool, IsVisitedF, P const &, X>>
+          std::vector<X> dijkstra(X start,
+                                  IsGoalF is_goal,
+                                  NeighborsF neighbors,
+                                  Q q = Q(),
+                                  P p = P(),
+                                  PushF push = bfs_impl::push<X>,
+                                  PopF pop = bfs_impl::pop<X>,
+                                  IsEmptyF is_empty = bfs_impl::isEmpty<X>,
+                                  VisitF visit = search_impl::visit<X>,
+                                  IsVisitedF is_visited =
+                                      search_impl::isVisited<X>)
+{
+  auto dist = std::unordered_map<X, I>();
+  auto push = [](Q &q_, X x_) { q_.insert(x_); };
+  auto pop = [&dist](Q &q) {
+    X x_ = std::ranges::min_element(
+        q, [&](X const &a, X const &b) { return dist[a] < dist[b]; });
+    q_.erase(x_);
+    return x_;
+  };
+  auto is_empty = [](const Q &q_) { return q_.empty(); };
+  auto visit = [&dist, &len](P &p_, X parent_, X child) {
+    dist[child_] =
+        dist.contains(parent) ? dist[parent_] + len(parent_, child_) : I{};
+    search_impl::visit(p_, parent_, child_);
+  };
+  auto is_visited = &search_impl::is_visited;
+  auto resolve_duplicates = [&dist, &len](Q &q_, P &p, X parent_, X child_) {
+    I d_child = dist[parent_] + len(parent_, child);
+    if (d_child < dist[child])
+    {
+      dist[child] = d_child;
+      search_impl::visit(p_, parent_, child_);
+    }
+  };
+  //////////////////////////////////////////////////////////////////////////////
+  auto [goal, p_res] = search(
+      q, p, start, push, pop, is_empty, is_goal, neighbors, visit, is_visited);
+
+  if (!is_goal(goal))
+    return {};
+
+  return trace(p_res, goal);
+}
+
 template <typename I,
           typename N,
           template <typename> typename C,
